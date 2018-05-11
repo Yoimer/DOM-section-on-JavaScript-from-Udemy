e1c0b41481c057cc32e810c4ef0a6dd11b8a03d2
Yoimer <ydrf19@gmail.com>
2018-05-11 12:35:51 -0400

    closures

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -246,14 +246,72 @@
 // Lecture :IIFE (ifi)
 // private variables
 
-// anonymous function, it has no name
-(function() {
-    var score = Math.random() * 10;
-    console.log(score >= 5);
-})();
+// // anonymous function, it has no name
+// (function() {
+//     var score = Math.random() * 10;
+//     console.log(score >= 5);
+// })();
 
-//passing parameter to an iffe function
-(function(goodLuck) {
-    var score = Math.random() * 10;
-    console.log(score >= 5 - goodLuck);
-})(5);
+// //passing parameter to an iffe function
+// (function(goodLuck) {
+//     var score = Math.random() * 10;
+//     console.log(score >= 5 - goodLuck);
+// })(5);
+
+//////////////////////////////////
+// Lecture Closures
+
+function retirement(retirementAge) {
+    var a = ' years left until retirement.';
+    return function (yearOfBirth) {
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
+
+// An inner function has always access to
+// the variables and parameters of its outer
+// function, even after the outer function
+// has returned.
+
+var retirementUS = retirement(66);
+retirementUS(1990);
+
+var retirementGermany = retirement(65);
+retirementGermany(1990);
+
+var retirementIceland = retirement(67);
+retirementIceland(1990);
+
+// retirement(66)(1990);
+
+// function interviewQuestion(job) {
+//     if (job === 'designer') {
+//         //anonymous function, it has no name
+//         return function(name) {
+//             console.log(name + ', can you please explain what UX design is?');
+//         }
+//     } else if (job === 'teacher') {
+//         return function(name) {
+//             console.log('What subject do you teach, ' + name + '?');
+//         }
+//     } else {
+//         return function(name) {
+//             console.log('Hello ' + name + ', ')
+//         }
+//     }
+// }
+
+function interviewQuestion (job) {
+    return function (name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log('What subject do you teach, ' + name + '?');
+        } else {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+interviewQuestion('teacher')('John');
