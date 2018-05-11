cd778b7526012d0cb1df27601abfaec75a7af8ab
Yoimer <ydrf19@gmail.com>
2018-05-11 19:20:46 -0400

    bind, call and apply

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -261,27 +261,27 @@
 //////////////////////////////////
 // Lecture Closures
 
-function retirement(retirementAge) {
-    var a = ' years left until retirement.';
-    return function (yearOfBirth) {
-        var age = 2016 - yearOfBirth;
-        console.log((retirementAge - age) + a);
-    }
-}
+// function retirement(retirementAge) {
+//     var a = ' years left until retirement.';
+//     return function (yearOfBirth) {
+//         var age = 2016 - yearOfBirth;
+//         console.log((retirementAge - age) + a);
+//     }
+// }
 
 // An inner function has always access to
 // the variables and parameters of its outer
 // function, even after the outer function
 // has returned.
 
-var retirementUS = retirement(66);
-retirementUS(1990);
+// var retirementUS = retirement(66);
+// retirementUS(1990);
 
-var retirementGermany = retirement(65);
-retirementGermany(1990);
+// var retirementGermany = retirement(65);
+// retirementGermany(1990);
 
-var retirementIceland = retirement(67);
-retirementIceland(1990);
+// var retirementIceland = retirement(67);
+// retirementIceland(1990);
 
 // retirement(66)(1990);
 
@@ -302,16 +302,94 @@ retirementIceland(1990);
 //     }
 // }
 
-function interviewQuestion (job) {
-    return function (name) {
-        if (job === 'designer') {
-            console.log(name + ', can you please explain what UX design is?');
-        } else if (job === 'teacher') {
-            console.log('What subject do you teach, ' + name + '?');
-        } else {
-            console.log('Hello ' + name + ', what do you do?');
+// function interviewQuestion (job) {
+//     return function (name) {
+//         if (job === 'designer') {
+//             console.log(name + ', can you please explain what UX design is?');
+//         } else if (job === 'teacher') {
+//             console.log('What subject do you teach, ' + name + '?');
+//         } else {
+//             console.log('Hello ' + name + ', what do you do?');
+//         }
+//     }
+// }
+
+// interviewQuestion('teacher')('John');
+
+//////////////////////////////////
+// Lecture BIND, CALL and APPLY.
+
+var john = {
+    name: 'John',
+    age: 26,
+    job: 'teacher',
+    presentation: function(style, timeOfDay) {
+        if (style === 'formal') {
+            console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+        } else if (style === 'friendly') {
+            console.log('Hey! What\'s up? I\'m ' + this.name + ', I\'m a ' + this.job + 'and I\'m ' + this.age + ' year old. Have a nice ' + timeOfDay + '.');
         }
     }
+};
+
+john.presentation('formal', 'morning');
+
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer'
+};
+// how could we use the presentation method for
+// the emily object since it does not have it?
+// let's start using the call method
+// the call method is AKA method borrowing
+john.presentation.call(emily, 'friendly', 'afternoon');
+
+// the apply method is similar to call
+// but apply method expects an array argument instead of a string
+// john.presentation.apply(emily, ['friendly', 'afternoon']);
+// this won't work since presentation method was not designed to recieve an array
+
+// bind method (returns a function)
+// similar to the call method as well,
+// it also allows to set the "this" variable explictly.
+// however the difference here is that bind doesn't inmmedialety
+// call the function, but instead, it generates a copy of the function
+// so then we can store it somewhere.
+// this can be so useful to create functions wth pre-set arguments
+
+// at this point we won't set the timeOfDay
+// this is what we call, currying in JS
+var johnFriendly = john.presentation.bind(john, 'friendly');
+johnFriendly('morning');
+johnFriendly('night');
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+emilyFormal('afternoon');
+
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    //The push() method adds new items to the end of an array, and returns the new length
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
 }
 
-interviewQuestion('teacher')('John');
+//el stands for element
+function calculateAge(el) {
+    return (2018 - el);
+}
+
+//returns true if and only if el is >=18
+function isFullAge(limit, el) {
+    return el >= limit;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+console.log(ages);
+console.log(fullJapan);
