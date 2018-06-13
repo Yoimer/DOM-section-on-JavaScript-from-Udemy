a018bd972cf11398e7ae90134012b325d58cbd01
Yoimer <ydrf19@gmail.com>
2018-06-13 18:12:28 -0400

    creating an initialization function

--- 6-Budgety/app2.js
+++ 6-Budgety/app2.js
@@ -38,11 +38,22 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     var DOM = UICtrl.getDOMstrings();
 
+    var setupEventListeners = function(){
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
+
+        document.addEventListener('keypress', function(event) {
+            // entert button was pressed
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
     var ctrlAddItem = function() {
-        //console.log('Button was clicked');
         // 1. Get the field input data
         var input = UICtrl.getinput();
-        console.log(input);
+        //console.log(input);
 
         // 2. Add the item to the budget controller
 
@@ -51,25 +62,15 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
-    }
+    };
 
-    //addEventListener calls ctrlAddItem for us
-    // document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
-
-    //addEventListener does not call ctrlAddItem() since ctrlAddItem() is a callback
-    // document.querySelector('.add__btn').addEventListener('click', ctrlAddItem())
-
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
-
-    // this happends on the global document, anywhere in the document
-    // the argument can have any name, I will use event
-    document.addEventListener('keypress', function(event) {
-        // entert button was pressed
-        if (event.keyCode === 13 || event.which === 13) {
-            // console.log('ENTER was pressed.');
-            ctrlAddItem();
+    return {
+        init: function() {
+            console.log('Application has started.');
+            setupEventListeners();
         }
-    });
+    };
 
 })(budgetController, UIController);
 
+controller.init();
