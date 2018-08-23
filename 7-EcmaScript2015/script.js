6696491e198a2981d83ed7c9a98b3dd86a4f53b5
Yoimer <ydrf19@gmail.com>
2018-08-23 00:30:41 -0400

    destructuring

---
+++ 7-EcmaScript2015/script.js
@@ -0,0 +1,40 @@
+///////////////////////////////////
+// Lecture : Destructuring
+
+// extracts all data that is saved in array in a single variable
+
+// ES5 (not practical when having a huge array length)
+// var john = ['John', 26];
+// var name = john[0];
+// var age = john[1];
+
+// ES6
+const [name, age] = ['John', 26];
+console.log(name);
+console.log(age);
+
+const obj = {
+    firstName: 'John',
+    lastName: 'Smith'
+};
+
+const {firstName, lastName} = obj;
+console.log(firstName);
+console.log(lastName);
+
+const {firstName: a, lastName: b} = obj;
+
+console.log(a);
+console.log(b);
+
+// another way to return multiple values
+// (avoiding returning an object)
+
+function calcAgeRetirement(year) {
+    const age = new Date().getFullYear() - year;
+    return [age, 65 - age];
+}
+
+const [age, retirement] = calcAgeRetirement(1990);
+console.log(age2);
+console.log(retirement);
