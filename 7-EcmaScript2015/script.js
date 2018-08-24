b8b94a0782c2298b413db7aeebaaa13ef481aad4
Yoimer <ydrf19@gmail.com>
2018-08-24 09:56:16 -0400

    the spread operator

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -42,53 +42,90 @@
 ///////////////////////////////////
 // Lecture : Arrays
 
-const boxes = document.querySelectorAll('.box');
+// const boxes = document.querySelectorAll('.box');
 
-//ES5
-var boxesArr5 = Array.prototype.slice.call(boxes);
+// //ES5
+// var boxesArr5 = Array.prototype.slice.call(boxes);
 
-// boxesArr5.forEach(function(cur) {
-//     cur.style.backgroundColor = 'dodgerblue';
-// });
+// // boxesArr5.forEach(function(cur) {
+// //     cur.style.backgroundColor = 'dodgerblue';
+// // });
 
-//ES6
-const boxesArr6 = Array.from(boxes);
-Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'dodgerblue');
+// //ES6
+// const boxesArr6 = Array.from(boxes);
+// Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'dodgerblue');
 
-//ES5
-// for(var i = 0; i < boxesArr5.length; i++){
-//     if(boxesArr5[i].className === 'box blue'){
+// //ES5
+// // for(var i = 0; i < boxesArr5.length; i++){
+// //     if(boxesArr5[i].className === 'box blue'){
+// //         continue;
+// //         // break;
+// //     }
+
+// //     boxesArr5[i].textContent = 'I changed to blue';
+
+// // }
+
+// //ES6
+// for(const cur of boxesArr6) {
+//     if(cur.className.includes('blue')) {
 //         continue;
-//         // break;
 //     }
-
-//     boxesArr5[i].textContent = 'I changed to blue';
-
+//     cur.textContent = 'I changed to blue!';
 // }
 
-//ES6
-for(const cur of boxesArr6) {
-    if(cur.className.includes('blue')) {
-        continue;
-    }
-    cur.textContent = 'I changed to blue!';
+// //ES5
+// var ages = [12, 17, 8, 21, 14, 11];
+
+// var full = ages.map(function(cur){
+//     return cur >= 18;
+// }); 
+// console.log(full);
+
+// console.log(full.indexOf(true));
+
+// console.log(ages[full.indexOf(true)]);
+
+// //ES6
+// // returns the index when cur is greater than or equal to 18(callback function)
+// console.log(ages.findIndex(cur => cur >= 18));
+
+// // returns the value when cur is greater than or equal to 18(callback function)
+// console.log(ages.find(cur => cur >= 18));
+
+///////////////////////////////////
+// Lecture : Spread Operator
+
+function addFourAges(a, b, c, d) {
+    return a + b +c +d;
 }
 
+var sum1 = addFourAges(18, 30, 12, 21);
+console.log(sum1);
+
 //ES5
-var ages = [12, 17, 8, 21, 14, 11];
-
-var full = ages.map(function(cur){
-    return cur >= 18;
-}); 
-console.log(full);
-
-console.log(full.indexOf(true));
-
-console.log(ages[full.indexOf(true)]);
+var ages = [18, 30, 12, 21];
+// apply method will take the ages array using the
+// addFourAges function using the elements of the array
+// as the arguments
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2);
 
 //ES6
-// returns the index when cur is greater than or equal to 18(callback function)
-console.log(ages.findIndex(cur => cur >= 18));
+//... is the spread operator
+//...ages means the same as writing 18,30,12,21 into addFourAges
+const sum3 = addFourAges(...ages);
+console.log(sum3);
 
-// returns the value when cur is greater than or equal to 18(callback function)
-console.log(ages.find(cur => cur >= 18));
+const familySmith = ['John', 'Jane', 'Mark'];
+const familyMiller = ['Mary', 'Bob', 'Ann'];
+// let spread familySmith and familyMiller
+const bigFamily = [...familySmith, 'Lily',...familyMiller];
+console.log(bigFamily);
+
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+// spreading h into boxes (boxes is a NodeList)
+const all = [h, ...boxes];
+// converting the spread (all) into an array
+Array.from(all).forEach(cur => cur.style.color = 'purple');
