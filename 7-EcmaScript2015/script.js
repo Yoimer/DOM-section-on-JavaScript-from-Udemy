869cc0f589f268224a43a76409b5bece565dbe56
Yoimer <ydrf19@gmail.com>
2018-08-26 16:45:45 -0400

    default parameters

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -164,19 +164,48 @@
 // rest parameter: it is used in the function declaration to accept an arbitrary number of parameters
 
 //ES5
-function isFullAge5(limit) {
-    var argsArr = Array.prototype.slice.call(arguments, 1);
+// function isFullAge5(limit) {
+//     var argsArr = Array.prototype.slice.call(arguments, 1);
 
-    argsArr.forEach(function(cur) {
-        console.log((2016 - cur) >= limit);
-    })
+//     argsArr.forEach(function(cur) {
+//         console.log((2016 - cur) >= limit);
+//     })
+// }
+
+// isFullAge5(21,1990, 1999, 1965);
+
+// //ES6
+// function isFullAge6(limit, ...years) {
+//     years.forEach(cur => console.log((2016 - cur )>= limit));
+// }
+
+// isFullAge6(1990, 1999, 1965, 2016, 1987);
+
+///////////////////////////////////
+// Lecture: Default Parameters
+/* 
+function SmithPerson(firstName, yearOfBirth, lastName, nationality){
+    
+    lastName === undefined ? lastName = 'Smith' : lastName = lastName;
+    nationality === undefined ? nationality = 'American' : nationality = nationality;
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;
+    this.nationality = nationality;
 }
 
-isFullAge5(21,1990, 1999, 1965);
+var john = new SmithPerson('John', 1990);
+
+var emily = new SmithPerson('Emily', 1983, 'Diaz', 'Spanish'); */
 
 //ES6
-function isFullAge6(limit, ...years) {
-    years.forEach(cur => console.log((2016 - cur )>= limit));
+function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'American') {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;
+    this.nationality = nationality;
 }
 
-isFullAge6(1990, 1999, 1965, 2016, 1987);
+var john = new SmithPerson('John', 1990);
+var emily = new SmithPerson('Emily', 1983, 'Diaz', 'Spanish');
