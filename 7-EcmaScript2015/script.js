550dbf46d69ac5cb50e845ac091457889141baf7
Yoimer <ydrf19@gmail.com>
2018-08-30 19:50:39 -0400

    variable declarations with let and const

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -371,92 +371,157 @@ var emily = new SmithPerson('Emily', 1983, 'Diaz', 'Spanish'); */
 // johnAthlete6.wonMedal();
 // johnAthlete6.calculateAge();
 
-///////////////////////////////////
+// ///////////////////////////////////
 // Lecture: Coding Challenge : Solution
 
-class Element {
-    constructor(name, buildYear) {
-        this.name = name;
-        this.buildYear = buildYear;
-    }
-}
+// class Element {
+//     constructor(name, buildYear) {
+//         this.name = name;
+//         this.buildYear = buildYear;
+//     }
+// }
 
-class Park extends Element {
-    constructor(name, buildYear, area, numTrees) {
-        // super class
-        super(name, buildYear);
-        this.area = area; //km2
-        this.numTrees = numTrees;
-    }
+// class Park extends Element {
+//     constructor(name, buildYear, area, numTrees) {
+//         // super class
+//         super(name, buildYear);
+//         this.area = area; //km2
+//         this.numTrees = numTrees;
+//     }
 
-    treeDensity() {
-        const density =  this.numTrees / this.area;
-        console.log(`${this.name} has a tree density of ${density} trees per square km.`);
-    }
-}
+//     treeDensity() {
+//         const density =  this.numTrees / this.area;
+//         console.log(`${this.name} has a tree density of ${density} trees per square km.`);
+//     }
+// }
 
-class Street extends Element {
-    constructor(name, buildYear, length, size = 3) {
-        super(name, buildYear);
-        this.length = length;
-        this.size = size;
-    }
+// class Street extends Element {
+//     constructor(name, buildYear, length, size = 3) {
+//         super(name, buildYear);
+//         this.length = length;
+//         this.size = size;
+//     }
 
-    classifyStreet() {
-        const classification = new Map();
-        classification.set(1, 'tiny');
-        classification.set(2, 'small');
-        classification.set(3, 'normal');
-        classification.set(4, 'big');
-        classification.set(5, 'huge');
-        console.log(`${this.name}, build in ${this.buildYear}, is a ${classification.get(this.size)} street.`);
-    }
-}
+//     classifyStreet() {
+//         const classification = new Map();
+//         classification.set(1, 'tiny');
+//         classification.set(2, 'small');
+//         classification.set(3, 'normal');
+//         classification.set(4, 'big');
+//         classification.set(5, 'huge');
+//         console.log(`${this.name}, build in ${this.buildYear}, is a ${classification.get(this.size)} street.`);
+//     }
+// }
 
-const allParks = [
-    new Park('Green Park', 1987, 0.2, 215),
-    new Park('National Park', 1894, 2.9,3541),
-    new Park('Oak Park', 1953, 0.4, 949)
-];
+// const allParks = [
+//     new Park('Green Park', 1987, 0.2, 215),
+//     new Park('National Park', 1894, 2.9,3541),
+//     new Park('Oak Park', 1953, 0.4, 949)
+// ];
 
-const allStreets = [
-    new Street('Ocean Avenue', 1999, 1.1, 4),
-    new Street('Evergreen Street', 2008, 2.7, 2),
-    new Street('4th Street', 2015, 0.8),
-    new Street('Sunset Boulevard', 1982, 2.5, 5)
-];
+// const allStreets = [
+//     new Street('Ocean Avenue', 1999, 1.1, 4),
+//     new Street('Evergreen Street', 2008, 2.7, 2),
+//     new Street('4th Street', 2015, 0.8),
+//     new Street('Sunset Boulevard', 1982, 2.5, 5)
+// ];
 
-function calc(arr) {
-    const sum = arr.reduce((prev, cur, index) => prev + cur, 0);    
-    return [sum, sum / arr.length];
-}
+// function calc(arr) {
+//     const sum = arr.reduce((prev, cur, index) => prev + cur, 0);    
+//     return [sum, sum / arr.length];
+// }
 
-function reportParks(p) {
-    console.log('-----PARKS REPORT-----');
+// function reportParks(p) {
+//     console.log('-----PARKS REPORT-----');
 
-    // Density
-    p.forEach(el => el.treeDensity());
+//     // Density
+//     p.forEach(el => el.treeDensity());
 
-    // Average age
-    const ages = p.map(el => new Date().getFullYear() - el.buildYear);
-    const [totalAge, avgAge] = calc(ages);
-    console.log(`Our ${p.length} parks have an average of ${avgAge} years.`);
+//     // Average age
+//     const ages = p.map(el => new Date().getFullYear() - el.buildYear);
+//     const [totalAge, avgAge] = calc(ages);
+//     console.log(`Our ${p.length} parks have an average of ${avgAge} years.`);
 
-    // Which park has more than 1000 trees
-    const i = p.map(el => el.numTrees).findIndex(el => el >= 1000);
-    console.log(`${p[i].name} has more than 1000 trees`);
-}
+//     // Which park has more than 1000 trees
+//     const i = p.map(el => el.numTrees).findIndex(el => el >= 1000);
+//     console.log(`${p[i].name} has more than 1000 trees`);
+// }
 
-function reportStreets(s) {
-    console.log('-----STREETS REPORT-----');
+// function reportStreets(s) {
+//     console.log('-----STREETS REPORT-----');
 
-    //Total and average length of the town's streets
-    const [totalLength, avgLength] = calc(s.map(el => el.length));
-    console.log(`Our ${s.length} streets have a total length of ${totalLength} km, with an average of ${avgLength} km.`);
+//     //Total and average length of the town's streets
+//     const [totalLength, avgLength] = calc(s.map(el => el.length));
+//     console.log(`Our ${s.length} streets have a total length of ${totalLength} km, with an average of ${avgLength} km.`);
     
-    // CLassify sizes
-    s.forEach(el => el.classifyStreet());
+//     // CLassify sizes
+//     s.forEach(el => el.classifyStreet());
+// }
+
+// reportParks(allParks);
+// reportStreets(allStreets);
+
+// ///////////////////////////////////
+// Lecture: let and const
+
+// ES5
+// var name5 = 'Jane Smith';
+// var age = 23;
+// // mutates
+// name5 = 'Jane Miller';
+// console.log(name5);
+
+// //ES6
+// const name6 = 'Jane Smith';
+// let age6 = 23;
+// name6 = 'Jane Miller';
+// console.log(name6);
+
+// var is function scoped
+// let is block scoped
+
+//ES5
+function driversLicense5(passedTest) {
+    if (passedTest) {
+        //hoisted
+        // console.log(firstName); (undefined)
+        var firstName = 'John';
+        var yearOfBirth = 1990;
+    }
+    // this will work
+    console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a card.');
 }
 
-reportParks(allParks);
-reportStreets(allStreets);
+// this won't work here. var is function scoped, and it is out its function
+// console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a card.');
+
+driversLicense5(true);
+
+//ES6
+function driversLicense6(passedTest) {
+    //console.log(firstName); not defined
+    let firstName;
+    const yearOfBirth = 1990;
+
+    if (passedTest) {
+
+        firstName = 'John';
+        // console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a card.');
+    }
+    // this won't work
+    console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a card.');
+}
+
+driversLicense6(true);
+
+// let i = 23;
+// for (let i = 0;  i < 5; i++) {
+//     console.log(i);
+// }
+// console.log(i);
+
+// var i = 23;
+// for (var i = 0;  i < 5; i++) {
+//     console.log(i);
+// }
+// console.log(i);
