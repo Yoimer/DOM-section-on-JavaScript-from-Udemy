7cf41033f75b7a40ce0d552924cc14ae7743e000
Yoimer <ydrf19@gmail.com>
2018-08-31 01:42:12 -0400

    arrow functions: lexical 'this' keyword

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -572,25 +572,102 @@ var emily = new SmithPerson('Emily', 1983, 'Diaz', 'Spanish'); */
 // ///////////////////////////////////
 // Lecture: Arrow functions
 
-const years = [1990, 1965, 1982, 1937];
+// const years = [1990, 1965, 1982, 1937];
+
+// //ES5
+// var ages5 = years.map(function(el) {
+//     return 2016 - el;
+// });
+// console.log(ages5);
+
+// //ES6
+// let ages6 = years.map(el => 2016 - el);
+// console.log(ages6);
+
+// //ES6 (using two arguments in .map method)
+// ages6 = years.map((el, index) => `Age element ${index}: ${2016 - el}`);
+// console.log(ages6);
+
+// ages6 = years.map((el, index) => {
+//     const now = new Date().getFullYear();
+//     const age = now - el;
+//     return `Age element ${index}: ${2016 - el}`;
+// });
+// console.log(ages6);
+
+// ///////////////////////////////////
+// Lecture: Arrow functions 2
 
 //ES5
-var ages5 = years.map(function(el) {
-    return 2016 - el;
-});
-console.log(ages5);
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+
+        // saves 'this' keyword from box5 to be later used in addEventListener callback function
+        var self = this;
+        document.querySelector('.green').addEventListener('click', function() {
+            // 'this' keyword no longer belongs to the previous 'this' from
+            // color and position in box5, we're using a callback function
+            var str = 'This is box number ' + self.position + ' and it is ' + self.color;
+            alert(str);
+        });
+    }
+}
+
+// box5.clickMe();
 
 //ES6
-let ages6 = years.map(el => 2016 - el);
-console.log(ages6);
+const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
 
-//ES6 (using two arguments in .map method)
-ages6 = years.map((el, index) => `Age element ${index}: ${2016 - el}`);
-console.log(ages6);
+        document.querySelector('.green').addEventListener('click', () => {
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+}
 
-ages6 = years.map((el, index) => {
-    const now = new Date().getFullYear();
-    const age = now - el;
-    return `Age element ${index}: ${2016 - el}`;
-});
-console.log(ages6);
+// box6.clickMe();
+
+//ES6
+// const box66 = {
+//     color: 'green',
+//     position: 1,
+//     clickMe: () => {
+
+//         document.querySelector('.green').addEventListener('click', () => {
+//             var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+//             alert(str);
+//         });
+//     }
+// }
+
+// box66.clickMe();
+
+function Person(name) {
+    this.name = name;
+}
+
+//ES5
+Person.prototype.myFriends5 = function(friends) {
+    var arr = friends.map(function(el) {
+        return this.name + ' is friends with ' + el;
+    });
+
+    console.log(arr);
+}
+
+var friends = ['Bob', 'Jane', 'Mark'];
+new Person('John').myFriends5(friends);
+
+// ES6
+Person.prototype.myFriends6 = function(friends) {
+    var arr = friends.map(el => `${this.name} is friends with ${el}`);
+
+    console.log(arr);
+}
+
+new Person('Mike').myFriends6(friends);
